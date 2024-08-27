6dc64490fb25ff7725987ee7aba84f740b244b7e
Vincent Prins <vincent.prins@ivdnt.org>
2024-08-27 10:25:40 +0200

    comma separated searches

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,25 +1,51 @@
 <template>
-    <input type="text" v-model="chartData.datasets[0].label" />
-    <input type="button" value="search" @click="bar.value.update()" />
+    <div class="search">
+        <p>wordform(s) (comma-separated):</p>
+        <form v-on:submit.prevent>
+            <input type="text" v-model="wordform" placeholder="wordform" />
+            <input type="submit" value="search" @click="search()" />
+        </form>
+        <p>options:</p>
+        <label>
+            <input type="checkbox" v-model="isAbsolute" />
+            absolute
+        </label>
+        <label>
+            <input type="checkbox" v-model="shouldZeroPad" />
+            zero pad
+        </label>
+    </div>
     <div class="container">
         <Line ref="bar" :data="chartData" :options="chartOptions" :styles />
     </div>
 </template>
+
 <script setup lang="ts">
-import { ref } from 'vue'
-import type { ChartOptions, ChartData } from 'chart.js'
+import { ref, watch } from 'vue'
+import type { ChartOptions, ChartData } from 'chart.js';
 import { nl } from 'date-fns/locale';
 import 'chartjs-adapter-date-fns';
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js/auto'
 import { Bar, Line } from 'vue-chartjs'
+
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 // https://stackblitz.com/github/apertureless/vue-chartjs/tree/main/sandboxes/reactive
+
+const isAbsolute = ref(true)
+const shouldZeroPad = ref(false)
+const wordform = ref('')
+const datasets = ref([])
+
 const bar = ref(null)
 const chartData = ref<ChartData<'line'>>({
     datasets: [
         {
             label: 'lopen',
             data: [{ x: 1619500411 * 1000, y: 40 }, { x: 1719500411 * 1000, y: 20 }],
+        },
+        {
+            label: 'zwevne',
+            data: [{ x: 1619500411 * 1000, y: 30 }, { x: 1719500411 * 1000, y: 60 }],
         },
     ],
 })
@@ -28,9 +54,16 @@ const chartOptions = ref<ChartOptions<'line'>>({
     responsive: true,
     maintainAspectRatio: false,
     animation: false,
-    color: '#0ff',
-    backgroundColor: '#f00',
-    borderColor: '#0F0',
+    // color: '#0ff',
+    // backgroundColor: '#f00',
+    // borderColor: '#0F0',
+    tooltip: {
+        callbacks: {
+            label: function (context) {
+                return context.dataset.label;
+            }
+        }
+    },
     scales: {
         // adapters: {
         //     date: {
@@ -39,9 +72,11 @@ const chartOptions = ref<ChartOptions<'line'>>({
         // },
         x: {
             type: 'time',
-            // time: {
-            //     unit: 'day',
-            // },
+
+            time: {
+                round: 'day',
+                tooltipFormat: 'dd-MM-yyyy',
+            },
             title: {
                 text: 'Date',
             },
@@ -54,43 +89,57 @@ const chartOptions = ref<ChartOptions<'line'>>({
             hitRadius: 20,
         },
     },
+    plugins: {
+        colors: {
+            forceOverride: true
+        }
+    }
 })
 const styles = ref({
     height: '300px',
     width: '100%',
     position: 'relative',
 })
-window.onclick = () => {
+
+function getFrequency(wordform) {
     // api call
-    const url = `http://localhost:8000/word_frequency?poshead=${chartData.value.datasets[0].label}&zero_pad=false`
+    const url = `http://localhost:8000/word_frequency?wordform=${wordform}&zero_pad=${shouldZeroPad.value}&absolute=${isAbsolute.value}`
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
             // data is in the form [{time: 1619500411, value: 40}, {time: 1719500411, value: 20}]
-            chartData.value.datasets[0].data = data.map((d) => {
-                return { x: d.time * 1000, y: d.frequency }
-            })
-
-            let copy = JSON.parse(JSON.stringify(chartData.value))
-            chartData.value = copy
+            const dataset = {
+                label: wordform,
+                data: data.map((d) => {
+                    return { x: d.time * 1000, y: d.frequency }
+                }),
+            }
+            datasets.value.push(dataset)
         })
-
-
 }
+
+function update(datasets) {
+    chartData.value.datasets = datasets
+    let copy = JSON.parse(JSON.stringify(chartData.value))
+    chartData.value = copy
+    // chartData.value = { "datasets": datasets }
+}
+
+function search() {
+    datasets.value = []
+    const wordforms = wordform.value.split(',').map((w) => w.trim())
+    wordforms.forEach((w) => getFrequency(w))
+}
+
+watch(datasets, () => update(datasets.value), { deep: true })
+
 </script>
 <style lang="scss">
-:deep(canvas) {
-    max-height: 400px;
-    max-width: 400px;
-    height: 400px;
-    width: 400px;
-}
-
 .container {
-    border: 10px solid red;
+    border: 1px solid gray;
     max-height: 400px;
-    max-width: 400px;
+    max-width: 800px;
     height: 400px;
-    width: 400px;
+    width: 800px;
 }
 </style>
