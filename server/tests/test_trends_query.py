bb7ef32498a6a6e0f1923ec84feec95f04045199
Vincent Prins <vincent.prins@ivdnt.org>
2024-11-22 12:25:00 +0100

    Example query unit test

--- server/tests/test_trends_query.py
+++ server/tests/test_trends_query.py
@@ -1,41 +1,38 @@
+# standard
 import unittest
-from unittest.mock import MagicMock
-from psycopg import Cursor
-from server.query.trends_query import TrendsQuery, TrendType
-from server.query.query_builder import ExecutableQuery
+
+# third party
+import psycopg
+
+# local
+from server.query.word_frequency_query import WordFrequencyQuery
+from server.config.connection import get_reader_conn_str
+from util.datatypes import DataSeries
+from util.dataseries_row_factory import DataSeriesRowFactory
 
 
-class TrendsQueryTest(unittest.TestCase):
-    def setUp(self):
-        self.cursor = MagicMock(spec=Cursor)
+class WordFrequencyQueryTest(unittest.IsolatedAsyncioTestCase):
+    async def asyncSetUp(self):
+        self.connection = await psycopg.AsyncConnection.connect(get_reader_conn_str())
+        self.cursor = self.connection.cursor(row_factory=DataSeriesRowFactory)
 
-    def test_init_default(self):
-        tq = TrendsQuery()
-        self.assertEqual(tq.time_span, "1 month")
-        self.assertEqual(tq.modifier, 1)
-        self.assertEqual(tq.trend_type, TrendType.ABSOLUTE)
+    async def asyncTearDown(self) -> None:
+        await self.cursor.close()
+        await self.connection.close()
 
-    def test_init_custom(self):
-        tq = TrendsQuery(
-            period_type="week", period_length=2, trend_type="keyness", modifier=5
+    async def test_init_default(self):
+        result: list[DataSeries] = (
+            await WordFrequencyQuery(
+                wordform="de",
+                period_type="year",
+                period_length=1,
+            )
+            .build(self.cursor)
+            .execute_fetchall()
         )
-        self.assertEqual(tq.time_span, "2 week")
-        self.assertEqual(tq.modifier, 5)
-        self.assertEqual(tq.trend_type, TrendType.KEYNESS)
-
-    def test_build_absolute_trends(self):
-        tq = TrendsQuery()
-        tq.trend_type = TrendType.ABSOLUTE
-        query = tq.build(self.cursor)
-        self.assertIsInstance(query, ExecutableQuery)
-        self.assertIn("SELECT", query.query.as_string(self.cursor))
-
-    def test_build_keyness_trends(self):
-        tq = TrendsQuery(trend_type="keyness")
-        tq.trend_type = TrendType.KEYNESS
-        query = tq.build(self.cursor)
-        self.assertIsInstance(query, ExecutableQuery)
-        self.assertIn("SELECT", query.query.as_string(self.cursor))
+        # grab the last year
+        last_year: DataSeries = result[-1]
+        assert last_year.rel_freq > 0.01
 
 
 if __name__ == "__main__":
