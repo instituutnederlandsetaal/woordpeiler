e738b7a8816d31d09689950dedfd29a1817b9e5a
Vincent Prins <vincent.prins@ivdnt.org>
2024-12-06 14:00:27 +0100

    updated logo colours

--- client/src/components/AppHeader.vue
+++ client/src/components/AppHeader.vue
@@ -2,7 +2,7 @@
     <header role="banner">
         <div class="logo">
             <a href="/">
-                <img src="@/assets/corpustrends-logo.svg" alt="logo" />
+                <img src="/corpustrends-logo.svg" alt="logo" />
             </a>
             <div class="logo-text">
                 <a href="https://ivdnt.org/" target="_blank" tabindex="-1" rel="noopener noreferrer">
@@ -25,8 +25,55 @@
 </template>
 
 <style scoped lang="scss">
-img {
-    width: 100px;
-    margin: 0 0.5rem;
+header {
+    font-family: Schoolboek;
+    height: 100px;
+    border-bottom: 0.4rem solid #4488ee;
+    display: flex;
+    justify-content: space-between;
+    background-color: white;
+    box-shadow: 0px 4px 5px 1px #ccc;
+
+    h1 {
+        font-weight: 400;
+        font-size: 2.5rem;
+        margin-top: -0.5rem;
+    }
+
+    h2 {
+        font-weight: 400;
+        font-size: 1rem;
+    }
+
+    a {
+        color: inherit;
+        text-decoration: none;
+    }
+
+    .logo {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        img {
+            min-width: 100px;
+            margin: 0 0.5rem;
+        }
+    }
+
+    nav {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        a {
+            margin: 0 1rem;
+            font-size: 1.2rem;
+
+            &:hover {
+                text-decoration: underline;
+            }
+        }
+    }
 }
 </style>
