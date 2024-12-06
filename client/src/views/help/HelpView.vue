4ec685d10721caff64811b7ee70d8114343aa9f7
Vincent Prins <vincent.prins@ivdnt.org>
2024-12-06 16:49:33 +0100

    Small start on help screen

--- client/src/views/help/HelpView.vue
+++ client/src/views/help/HelpView.vue
@@ -1,21 +1,130 @@
 <template>
     <Panel class="helpPanel">
-        <h2>Basiszoeken</h2>
-        <p>Klik op het plusje om een nieuwe zoekopdracht toe te voegen.</p>
+        <section>
+            <h2>Basiszoeken</h2>
+            <ul>
+                <li>
+                    <b>Toevoegen:</b>
+                    klik op
+                    <Button class="dummyNewWord" severity="secondary" title="Zoekterm toevoegen" outlined>
+                        <span class="pi pi-plus"></span>
+                    </Button>
+                    om een nieuwe zoekopdracht toe te voegen.
+                </li>
+                <li>
+                    <b>Woord:</b>
+                    het woord waarop u wilt zoeken.
+                    Niet hoofdletter- of accentgevoelig:
+                    met <i>belgie</i> vindt u ook <i>België</i>.
+                </li>
+                <li>
+                    <b>Taalvariëteit:</b>
+                    kies uit de lijst een taalvariëteit van het Nederlands waarin u het woord wilt zoeken.
+                </li>
+                <li>
+                    <b>In- en uitklappen:</b>
+                    klik op <span class="pi pi-plus"></span> of <span class="pi pi-minus"></span> om een zoekterm in de
+                    lijst te in of uit te klappen.
+                </li>
+                <li>
+                    <b>Verwijderen:</b>
+                    klik op <span class="pi pi-trash"></span> om een zoekterm uit de lijst te verwijderen.
+                </li>
+                <li>
+                    <b>Zichtbaarheid:</b>
+                    klik op <span class="pi pi-eye"></span> of <span class="pi pi-eye-slash"></span> om een zoekterm in
+                    de
+                    grafiek te verbergen of te tonen.
+                </li>
+                <li>
+                    <b>Kleur:</b>
+                    klik op
+                    <ColorPicker /> om de kleur van een zoekterm in de grafiek aan te passen.
+                </li>
+            </ul>
+        </section>
+        <section>
+            <h2>Grafiek</h2>
+            <ul>
+                <li>
+                    <b>Inzoomen:</b>
+                    zoom in op een periode door over de grafiek te slepen.
+                </li>
+                <li>
+                    <b>Uitzoomen:</b>
+                    dubbelklik op de grafiek om uit te zoomen.
+                </li>
+                <li>
+                    <b>Downloaden:</b>
+                    klik op <span class="pi pi-download"></span> om de grafiek als afbeelding te downloaden.
+                </li>
+            </ul>
+        </section>
+
+        <section>
+            <h2>Zoekinstellingen</h2>
+            <ul>
+                <li>
+                    <b>Periode:</b>
+                    klik op <span class="pi pi-calendar"></span> om de periode van de zoekopdracht aan te passen.
+                </li>
+            </ul>
+        </section>
+
     </Panel>
 </template>
 
 <script setup lang="ts">
 import Panel from 'primevue/panel';
+import ColorPicker from 'primevue/colorpicker';
 </script>
 
 <style scoped lang="scss">
 .helpPanel {
-    margin: 1rem;
     width: 100%;
 
     :deep(.p-panel-header) {
-        padding: 0.3rem !important;
+        padding: 0.7rem !important;
+    }
+
+    :deep(.p-panel-content) {
+        padding: 0 2rem !important;
+    }
+
+    section {
+        margin-bottom: 1rem;
+    }
+}
+
+.dummySearchSettings {
+    max-width: 500px;
+}
+
+span.pi,
+.p-colorpicker {
+    padding: 0 0.2rem;
+}
+
+.dummyNewWord {
+    border: 2px dashed #ccc;
+    background: #eee;
+    width: 60px;
+
+    span {
+        font-size: 0.8rem;
+    }
+}
+
+ul {
+    list-style-type: none;
+    padding-left: 0;
+}
+
+li {
+    margin-bottom: 0.4rem;
+
+    b {
+        margin-right: 0.25rem;
     }
 }
 </style>
