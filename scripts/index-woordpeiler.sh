64beb05788369648d13fccb50814a94c868c7fb2
Vincent Prins <vincent.prins@ivdnt.org>
2025-01-13 12:14:01 +0100

    Execute frequency tool directly
    using another subscript feels overly complex

--- scripts/index-woordpeiler.sh
+++ scripts/index-woordpeiler.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -o errexit # Exit on error (set -e)
 
-# remove the previous output
-WOORDPEILER_OUTPUT=/vol1/blacklab-util/FrequencyTool/tmp/
-rm -rf WOORDPEILER_OUTPUT
-# run the frequency tool
-/vol1/blacklab-util/FrequencyTool/calc-woordpeiler.sh
+# remove the previous output and run the frequency tool
+WOORDPEILER_INPUT=/vol2/blacklab-indices/chn-intern/ # should this be vol1?
+WOORDPEILER_DIR=/vol1/blacklab-util/FrequencyTool/
+WOORDPEILER_OUTPUT="$WOORDPEILER_DIR/tmp/" # currently defaults to tmp/
+WOORDPEILER_CONFIG="$WOORDPEILER_DIR/unigram.yaml" # only index unigrams for now
+rm -rf $WOORDPEILER_OUTPUT
+java -Xmx48G -cp "$WOORDPEILER_DIR/blacklab-tools.jar:lib/*" nl.inl.blacklab.tools.frequency.FrequencyTool --no-merge $WOORDPEILER_INPUT $WOORDPEILER_CONFIG $WOORDPEILER_DIR
 
 # copy the results to the woordpeiler servers and create databases
-WOORDPEILER_DEV=svowct01.ivdnt.loc # will later be svowwr01.ivdnt.loc
+WOORDPEILER_DEV=corpustrends.dev.ivdnt.loc # will later be svowwr01.ivdnt.loc
 WOORDPEILER_PROD=svatwr01.ivdnt.loc # will later be svprwr01.ivdnt.loc
 WOORDPEILER_DEST=/vol1/tsv/
 ./update-woordpeiler-server.sh $WOORDPEILER_OUTPUT $WOORDPEILER_DEV $WOORDPEILER_DEST
