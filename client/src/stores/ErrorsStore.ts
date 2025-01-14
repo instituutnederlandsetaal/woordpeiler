ec5c4660714605ed028ce51458ffc3095e8cc94d
Vincent Prins <vincent.prins@ivdnt.org>
2025-01-14 14:49:17 +0100

    we could also display the error detail
    But let's not do it for now. Less is more

--- client/src/stores/ErrorsStore.ts
+++ client/src/stores/ErrorsStore.ts
@@ -10,7 +10,12 @@ export const useErrorsStore = defineStore('Errors', () => {
         axios.interceptors.response.use(
             response => response,
             error => {
-                const msg = `${error.config.url.slice(1)}: ${error.message}`
+                let msg = `${error.config.url.slice(1)}: ${error.message}`
+                // If we ever decide to use the detail, uncomment this:
+                // const detail = error.response?.data?.detail
+                // if (detail) {
+                //     msg = `${msg}: ${detail}`
+                // }
                 errors.value.push(msg)
                 return Promise.reject(error)
             }
