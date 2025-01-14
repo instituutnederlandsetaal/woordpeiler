cc90325bd369575e80d0b522d92a9b8b6dfc35d9
Vincent Prins <vincent.prins@ivdnt.org>
2025-01-14 14:46:22 +0100

    Added proxy

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,7 +1,13 @@
 import axios from 'axios'
 
 export function setAxiosBaseUrl() {
-    axios.defaults.baseURL = `${window.location.protocol}//${window.location.hostname}:8000/`
+    // On local development, use localhost:8000
+    // On production, use host /api
+    if (window.location.hostname == 'localhost') {
+        axios.defaults.baseURL = `${window.location.protocol}//${window.location.hostname}:8000/`
+    } else {
+        axios.defaults.baseURL = `${window.location.protocol}//${window.location.hostname}/api/`
+    }
 }
 
 export function cleanParams(params: any) {
