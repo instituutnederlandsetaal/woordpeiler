ee400d0b1edde5d86eda439762915a0b784e7d98
Vincent Prins <vincent.prins@ivdnt.org>
2025-01-17 13:47:17 +0100

    log client access

--- client/server.js
+++ client/server.js
@@ -1,9 +1,15 @@
-const express = require('express');
+const express = require('express')
 const history = require('connect-history-api-fallback')
 
-const app = express();
-const port = 8080;
+const app = express()
+const port = 8080
 const staticFileMiddleware = express.static('dist')
+
+// log access
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`)
+    next()
+})
 
 // Note!: use history should come before use static middleware. Otherwise subpages don't load.
 app.use(history({ index: '/index.html' }))
