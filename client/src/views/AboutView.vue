0e11a346230a839adc21be431dcf51f254301c55
Vincent Prins <vincent.prins@ivdnt.org>
2025-01-20 13:04:08 +0100

    Added tabbed help screen

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -2,18 +2,24 @@
     <main>
         <Panel class="textPanel">
             <section>
-                <h2>Over Woordpeiler</h2>
+                <h2>Over de applicatie</h2>
                 <p>
-
+                    <i>Woordpeiler</i> is gemaakt door het Instituut voor de Nederlandse Taal (INT). De website maakt
+                    gebruik
+                    van de data uit het Corpus Hedendaags Nederlands (CHN), en voor onderzoekers is er vanuit
+                    <i>Woordpeiler</i> een directe koppeling naar het CHN om gedetailleerde (meta)data te kunnen
+                    bekijken.
                 </p>
-                <h3>Bronmateriaal</h3>
+            </section>
+            <section>
+                <h2>Bronmateriaal</h2>
                 <p>
                     De gegevens in <i>Woordpeiler</i> komen uit het krantenmateriaal van het
                     <a href="https://ivdnt.org/corpora-lexica/corpus-hedendaags-nederlands/">
                         Corpus Hedendaags Nederlands
                     </a>.
                 </p>
-                <h4>Samenstelling</h4>
+                <h3>Samenstelling</h3>
                 <ul>
                     <li>
                         <i>Antilliaans-Nederlands:</i>
@@ -21,12 +27,14 @@
                     </li>
                     <li>
                         <i>Belgisch-Nederlands:</i>
-                        De Morgen, De Standaard, Gazet van Antwerpen, Het Belang van Limburg, Het Laatste Nieuws,
+                        De Morgen, De Standaard, Gazet van Antwerpen, Het Belang van Limburg, Het Laatste
+                        Nieuws,
                         Het Nieuwsblad, Wablieft, WAI-NOT Krant.
                     </li>
                     <li>
                         <i>Nederlands-Nederlands:</i>
-                        ANP, Algemeen Dagblad, Dordrechts Dagblad, Elsevier, De Groene Amsterdammer, de Volkskrant,
+                        ANP, Algemeen Dagblad, Dordrechts Dagblad, Elsevier, De Groene Amsterdammer, de
+                        Volkskrant,
                         HP/De Tijd, Het Parool, Meppeler Courant, NRC, Start!-krant, Trouw, Vrij Nederland.
                     </li>
                     <li>
@@ -37,34 +45,35 @@
                 <p>
                 </p>
             </section>
+            <div class="logos">
+                <a href="https://www.ivdnt.org">
+                    <img src="@/assets/ivdNt-logo-3regels.svg" alt="Logo Instituut voor de Nederlandse taal"
+                        title="Instituut voor de Nederlandse taal" />
+                </a>
+            </div>
         </Panel>
     </main>
 </template>
 
 <script setup lang="ts">
 import Panel from 'primevue/panel';
-import ColorPicker from 'primevue/colorpicker';
-
-const canShare = navigator.share != undefined;
 </script>
 
 <style scoped lang="scss">
-.dummySearchSettings {
-    max-width: 500px;
-}
+.logos {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 3rem;
 
-span.pi,
-.p-colorpicker {
-    padding: 0 0.2rem;
-}
+    a {
+        display: block;
+        width: 100%;
+        max-width: 200px;
 
-.dummyNewWord {
-    border: 2px dashed #ccc;
-    background: #eee;
-    width: 60px;
-
-    span {
-        font-size: 0.8rem;
+        img {
+            width: 100%;
+        }
     }
 }
 </style>
--- client/src/views/HelpView.vue
+++ client/src/views/HelpView.vue
@@ -1,127 +1,207 @@
 <template>
     <main>
-        <Panel class="textPanel">
-            <section>
-                <h2>Basiszoeken</h2>
-                <ul>
-                    <li>
-                        <b>Woord:</b>
-                        het woord waarop u zoekt is niet hoofdletter- of accentgevoelig: met
-                        <RouterLink to="/grafiek?w=tiktok">tiktok</RouterLink> vindt u ook TikTok
-                        en met <RouterLink to="/grafiek?w=belgie">belgie</RouterLink> vindt u ook België.
-                    </li>
-                    <li>
-                        <b>Taalvariëteit:</b>
-                        standaard staat de grafiek ingesteld op alle taalvariëteiten van het Nederlands die in het
-                        krantenmateriaal vertegenwoordigd zijn. Wilt u alleen de data uit een specifieke taalvariëteit
-                        zien? Kies de variëteit in dit menu. Door op het kruisje te klikken verwijdert u de selectie
-                        weer. Vergeet niet opnieuw op "Zoeken" te klikken.
-                    </li>
-                    <li>
-                        <b>Kleur:</b>
-                        de kleur van de zoekterm in de grafiek is aan te passen door op het gekleurde blokje
-                        <ColorPicker /> te klikken. Dit is vooral nuttig als u op verschillende termen tegelijk zoekt.
-                    </li>
-                    <li>
-                        <b>Zichtbaarheid:</b>
-                        door op het oogje <span class="pi pi-eye"></span> te klikken kunt u een zoekterm in de grafiek
-                        tonen (default) of verbergen. Dit is vooral nuttig als u op verschillende termen tegelijk zoekt.
-                    </li>
-                    <li>
-                        <b>Verwijderen:</b>
-                        klik op het prullenbakje <span class="pi pi-trash"></span> om een zoekterm uit de lijst te
-                        verwijderen.
-                    </li>
-                    <li>
-                        <b>In- en uitklappen:</b>
-                        klik op <span class="pi pi-minus"></span> om een zoekterm in de lijst in- of uit te klappen.
-                    </li>
-                    <li>
-                        <b>Toevoegen:</b>
-                        klik op de
-                        <Button class="dummyNewWord" severity="secondary" title="Zoekterm toevoegen" outlined>
-                            <span class="pi pi-plus"></span>
-                        </Button>
-                        om een nieuwe zoekterm toe te voegen aan de grafiek.
-                    </li>
-                </ul>
-            </section>
+        <div class="fakePanel">
 
-            <section>
-                <h2>Zoekinstellingen</h2>
-                <p>
-                    Linksonder in de grafiekview vindt u de zoekinstellingen. Klik op <span
-                        class="pi pi-chevron-down"></span> om de instellingen te openen.
-                </p>
-                <ul>
-                    <li>
-                        <b>Splits automatisch op taalvariëteit:</b>
-                        vink deze optie aan om de zoekterm in de grafiek automatisch op alle taalvariëteiten te
-                        splitsen. De variëteiten krijgen verschillende kleuren die terug te vinden zijn in de legenda
-                        linksboven in de grafiek.
-                    </li>
-                    <li>
-                        <b>Frequentie:</b>
-                        kies tussen <i>absolute frequentie</i> (het totaal aantal voorkomens) of
-                        <i>relatieve frequentie</i> (het aantal voorkomens per 1 miljoen woorden).
-                    </li>
-                    <li>
-                        <b>Periode:</b>
-                        kies een zoekperiode in de tijd.
-                        De knop <span class="pi pi-refresh"></span> zet de periode terug op de standaardinstelling
-                        <i>2000 &ndash; nu</i>.
-                    </li>
-                    <li>
-                        <b>Interval:</b>
-                        dit is het interval waarover de frequentie wordt berekend.
-                        Bijvoorbeeld: <i>per 1 jaar</i>, <i>per 2 maanden</i> of <i>per 3 weken</i>.
-                        Standaard staat deze instelling op per 3 maanden.
-                    </li>
+            <Tabs value="0" class="textPanel">
+                <TabList>
+                    <Tab value="0">Basiszoeken</Tab>
+                    <Tab value="1">Algemeen</Tab>
+                </TabList>
+                <TabPanels>
+                    <TabPanel value="0">
+                        <section>
+                            <h2>Basiszoeken</h2>
+                            <ul>
+                                <li>
+                                    <b>Woord:</b>
+                                    het woord waarop u zoekt is niet hoofdletter- of accentgevoelig: met
+                                    <RouterLink to="/grafiek?w=tiktok">tiktok</RouterLink> vindt u ook TikTok
+                                    en met <RouterLink to="/grafiek?w=belgie">belgie</RouterLink> vindt u ook België.
+                                </li>
+                                <li>
+                                    <b>Taalvariëteit:</b>
+                                    standaard staat de grafiek ingesteld op alle taalvariëteiten van het Nederlands die
+                                    in
+                                    het
+                                    krantenmateriaal vertegenwoordigd zijn. Wilt u alleen de data uit een specifieke
+                                    taalvariëteit
+                                    zien? Kies de variëteit in dit menu. Door op het kruisje te klikken verwijdert u de
+                                    selectie
+                                    weer. Vergeet niet opnieuw op "Zoeken" te klikken.
+                                </li>
+                                <li>
+                                    <b>Kleur:</b>
+                                    de kleur van de zoekterm in de grafiek is aan te passen door op het gekleurde blokje
+                                    <ColorPicker /> te klikken. Dit is vooral nuttig als u op verschillende termen
+                                    tegelijk
+                                    zoekt.
+                                </li>
+                                <li>
+                                    <b>Zichtbaarheid:</b>
+                                    door op het oogje <span class="pi pi-eye"></span> te klikken kunt u een zoekterm in
+                                    de
+                                    grafiek
+                                    tonen (default) of verbergen. Dit is vooral nuttig als u op verschillende termen
+                                    tegelijk zoekt.
+                                </li>
+                                <li>
+                                    <b>Verwijderen:</b>
+                                    klik op het prullenbakje <span class="pi pi-trash"></span> om een zoekterm uit de
+                                    lijst
+                                    te
+                                    verwijderen.
+                                </li>
+                                <li>
+                                    <b>In- en uitklappen:</b>
+                                    klik op <span class="pi pi-minus"></span> of <span class="pi pi-plus"></span> om een
+                                    zoekterm in de lijst in- of uit te
+                                    klappen.
+                                </li>
+                                <li>
+                                    <b>Toevoegen:</b>
+                                    klik op de
+                                    <Button class="dummyNewWord" severity="secondary" title="Zoekterm toevoegen"
+                                        outlined>
+                                        <span class="pi pi-plus"></span>
+                                    </Button>
+                                    om een nieuwe zoekterm toe te voegen aan de grafiek.
+                                </li>
+                            </ul>
+                        </section>
 
-                </ul>
-            </section>
+                        <section>
+                            <h2>Zoekinstellingen</h2>
+                            <p>
+                                Linksonder in de grafiekview vindt u de zoekinstellingen. Klik op <span
+                                    class="pi pi-chevron-down"></span> om de instellingen te openen.
+                            </p>
+                            <ul>
+                                <li>
+                                    <b>Splits automatisch op taalvariëteit:</b>
+                                    vink deze optie aan om de zoekterm in de grafiek automatisch op alle taalvariëteiten
+                                    te
+                                    splitsen. De variëteiten krijgen verschillende kleuren die terug te vinden zijn in
+                                    de
+                                    legenda
+                                    linksboven in de grafiek.
+                                </li>
+                                <li>
+                                    <b>Frequentie:</b>
+                                    kies tussen <i>absolute frequentie</i> (het totaal aantal voorkomens) of
+                                    <i>relatieve frequentie</i> (het aantal voorkomens per 1 miljoen woorden).
+                                </li>
+                                <li>
+                                    <b>Periode:</b>
+                                    kies een zoekperiode in de tijd.
+                                    De knop <span class="pi pi-refresh"></span> zet de periode terug op de
+                                    standaardinstelling
+                                    <i>2000 &ndash; nu</i>.
+                                </li>
+                                <li>
+                                    <b>Interval:</b>
+                                    dit is het interval waarover de frequentie wordt berekend.
+                                    Bijvoorbeeld: <i>per 1 jaar</i>, <i>per 2 maanden</i> of <i>per 3 weken</i>.
+                                    Standaard staat deze instelling op per 3 maanden.
+                                </li>
 
-            <section>
-                <h2>Grafiek</h2>
-                <p>In de grafiek kunt u op een bepaalde periode inzoomen, en u kunt de grafiek als afbeelding
-                    downloaden.</p>
-                <ul>
-                    <li>
-                        <b>Inzoomen:</b>
-                        zoom in op een periode door met de cursor over de grafiek te slepen (houd de linkermuisknop
-                        ingedrukt). Er verschijnt een grijs selectievak en zodra u de muisknop loslaat zoomt u in op dit
-                        deel van de grafiek.
-                    </li>
-                    <li>
-                        <b>Uitzoomen:</b>
-                        dubbelklik op de grafiek om weer uit te zoomen.
-                    </li>
-                    <li>
-                        <b>Downloaden:</b>
-                        klik op de downloadknop <span class="pi pi-download"></span> om de grafiek als afbeelding te
-                        downloaden.
-                    </li>
-                    <li v-if="canShare">
-                        <b>Delen:</b>
-                        klik op de deelknop <span class="pi pi-share-alt"></span> om de grafiek te delen via de
-                        interface van
-                        Android /
-                        iOS.
-                    </li>
-                </ul>
-            </section>
-        </Panel>
+                            </ul>
+                        </section>
+
+                        <section>
+                            <h2>Grafiek</h2>
+                            <p>In de grafiek kunt u op een bepaalde periode inzoomen, en u kunt de grafiek als
+                                afbeelding
+                                downloaden.</p>
+                            <ul>
+                                <li>
+                                    <b>Inzoomen:</b>
+                                    zoom in op een periode door met de cursor over de grafiek te slepen (houd de
+                                    linkermuisknop
+                                    ingedrukt). Er verschijnt een grijs selectievak en zodra u de muisknop loslaat zoomt
+                                    u
+                                    in op dit
+                                    deel van de grafiek.
+                                </li>
+                                <li>
+                                    <b>Uitzoomen:</b>
+                                    dubbelklik op de grafiek om weer uit te zoomen.
+                                </li>
+                                <li>
+                                    <b>Downloaden:</b>
+                                    klik op de downloadknop <span class="pi pi-download"></span> om de grafiek als
+                                    afbeelding te
+                                    downloaden.
+                                </li>
+                                <li v-if="canShare">
+                                    <b>Delen:</b>
+                                    klik op de deelknop <span class="pi pi-share-alt"></span> om de grafiek te delen via
+                                    de
+                                    interface van
+                                    Android /
+                                    iOS.
+                                </li>
+                            </ul>
+                        </section>
+                    </TabPanel>
+                    <TabPanel value="1">
+                        <section>
+                            <h2>Algemeen</h2>
+                            <p>
+                                Met Woordpeiler kun je de populariteit van woorden door de tijd heen bekijken, op basis
+                                van teksten uit Nederlandstalige kranten. Zoek op een woord en Woordpeiler visualiseert
+                                de frequentie ervan door de jaren heen in een grafiek, vanaf 2000 tot nu.
+
+                                Het woord <RouterLink to="/grafiek?w=vapen">vapen</RouterLink> kwam bijvoorbeeld in 2014
+                                voor het eerst een enkele keer voor, maar
+                                vanaf 2022 is er een duidelijke stijging te zien van het gebruik van dit woord in de
+                                kranten. De woorden <RouterLink to="/grafiek?w=mondkapje,mondmasker">mondkapje en
+                                    mondmasker</RouterLink> kenden een opvallende piek van 2020 tot 2022
+                                en voor het woord <RouterLink to="/grafiek?w=televisie">televisie</RouterLink> is een
+                                dalende trend zichtbaar.
+                            </p>
+                        </section>
+
+                    </TabPanel>
+                </TabPanels>
+            </Tabs>
+        </div>
     </main>
 </template>
 
 <script setup lang="ts">
 import Panel from 'primevue/panel';
 import ColorPicker from 'primevue/colorpicker';
+import Tabs from 'primevue/tabs';
+import TabList from 'primevue/tablist';
+import Tab from 'primevue/tab';
+import TabPanels from 'primevue/tabpanels';
+import TabPanel from 'primevue/tabpanel';
 
 const canShare = navigator.share != undefined;
 </script>
 
 <style scoped lang="scss">
+main {
+    height: fit-content;
+    flex: 1;
+
+    .fakePanel {
+
+        display: flex;
+        justify-content: center;
+        border: 1px solid #e2e8f0;
+        background: white;
+        width: 100%;
+        flex: 1;
+
+        min-height: 100%;
+
+        .textPanel {
+            max-width: 1000px;
+        }
+    }
+}
+
 span.pi,
 .p-colorpicker {
     padding: 0 0.2rem;
