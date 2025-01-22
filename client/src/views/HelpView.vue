8c130a278799830bf93d17795e48fdb98ca2713a
Vincent Prins <vincent.prins@ivdnt.org>
2025-01-22 17:16:22 +0100

    Updated help

--- client/src/views/HelpView.vue
+++ client/src/views/HelpView.vue
@@ -4,11 +4,63 @@
 
             <Tabs value="0" class="textPanel">
                 <TabList>
-                    <Tab value="0">Basiszoeken</Tab>
-                    <Tab value="1">Algemeen</Tab>
+                    <Tab value="0">Algemeen</Tab>
+                    <Tab value="1">Basiszoeken</Tab>
                 </TabList>
                 <TabPanels>
                     <TabPanel value="0">
+                        <section>
+                            <h2>Algemeen</h2>
+                            <p>
+                                Met <i>Woordpeiler</i> kunt u bekijken hoe vaak woorden door de tijd heen voorkomen in
+                                teksten
+                                uit Nederlandstalige kranten. Zoek op een woord en <i>Woordpeiler</i> visualiseert de
+                                frequentie ervan door de jaren heen in een grafiek, vanaf 2000 tot nu.
+                            </p>
+                            <p>
+                                Het woord <RouterLink to="/grafiek?w=vapen">vapen</RouterLink> kwam bijvoorbeeld in 2014
+                                voor het eerst een enkele keer voor, maar
+                                vanaf 2022 is er een duidelijke stijging te zien van het gebruik van dit woord in de
+                                kranten. De woorden <RouterLink to="/grafiek?w=mondkapje,mondmasker">mondkapje en
+                                    mondmasker</RouterLink> kenden een opvallende piek van 2020 tot 2022
+                                en voor het woord <RouterLink to="/grafiek?w=televisie">televisie</RouterLink> is een
+                                dalende trend zichtbaar. Deze trends weerspiegelen
+                                niet alleen maatschappelijke en technologische ontwikkelingen (vapen, televisie), maar
+                                ook veranderingen in woordkeuze (zie bijvoorbeeld <RouterLink
+                                    to="/grafiek?w=videogame,videospel">videogame en videospel</RouterLink>) of de
+                                verschillen tussen de taalvariëteiten (mondkapje/mondmasker).
+                            </p>
+                            <p>
+                                De grafiek staat standaard ingesteld op de default-instellingen van de zoekfunctie. Er
+                                zijn verschillende aanpassingen mogelijk, die beschreven staan bij het tabje
+                                <i>Basiszoeken</i>.
+                            </p>
+                            <p>
+                                Hieronder staan de visualisaties van de bovengenoemde voorbeelden als voorbeeld. Op de
+                                x-as is de tijd weergegeven in jaren, maanden of dagen. De y-as toont de
+                                woordfrequentie.
+                            </p>
+                        </section>
+
+                        <div class="image">
+                            <img src="@/assets/img/vapen.png" style="max-width: 800px;"
+                                alt="Grafiek van de woordfrequentie van vapen" />
+                        </div>
+                        <div class="image">
+                            <img src="@/assets/img/mondkapje.png" style="max-width: 800px;"
+                                alt="Grafiek van de woordfrequenties van mondkapje en mondmasker" />
+                        </div>
+                        <div class="image">
+                            <img src="@/assets/img/videospel.png" style="max-width: 800px;"
+                                alt="Grafiek van de woordfrequenties van videospel en videogame" />
+                        </div>
+                        <div class="image">
+                            <img src="@/assets/img/televisie.png" style="max-width: 800px;"
+                                alt="Grafiek van de woordfrequentie van televisie" />
+                        </div>
+
+                    </TabPanel>
+                    <TabPanel value="1">
                         <section>
                             <h2>Basiszoeken</h2>
                             <ul>
@@ -53,7 +105,8 @@
                                 </li>
                                 <li>
                                     <b>In- en uitklappen:</b>
-                                    klik op <span class="pi pi-minus"></span> of <span class="pi pi-plus"></span> om een
+                                    klik op <span class="pi pi-chevron-up"></span> of <span
+                                        class="pi pi-chevron-down"></span> om een
                                     zoekterm in de lijst in- of uit te
                                     klappen.
                                 </li>
@@ -133,7 +186,8 @@
                                 </li>
                                 <li>
                                     <b>Uitzoomen:</b>
-                                    dubbelklik op de grafiek om weer uit te zoomen.
+                                    dubbelklik op de grafiek om weer uit te zoomen of klik op de uitzoomknop <span
+                                        class="pi pi-search-minus"></span>.
                                 </li>
                                 <li>
                                     <b>Downloaden:</b>
@@ -144,32 +198,33 @@
                                 <li v-if="canShare">
                                     <b>Delen:</b>
                                     klik op de deelknop <span class="pi pi-share-alt"></span> om de grafiek te delen via
+                                    de interface van Android / iOS.
+                                </li>
+                                <li>
+                                    <b>Punten in de curve:</b> als u met uw muis over een punt in de curve gaat,
+                                    verschijnt een
+                                    tekstvakje met een samenvatting van de corpusgegevens waarvoor het punt staat: het
+                                    woord, de datum en de relatieve of absolute frequentie. Onderzoekers kunnen ook
+                                    doorklikken om de gegevens voor dat punt in het CHN te bekijken. Let wel, bij een
+                                    tijdsindeling in weken of met intervallen voor jaar, maand of dag niet gelijk aan 1,
+                                    worden de gegevens in CHN met een kleinere datumgranulariteit weergegeven.
+                                </li>
+                                <li>
+                                    <b>Legenda:</b> voor elke curve in de grafiek wordt de kleur, het woord en eventueel
                                     de
-                                    interface van
-                                    Android /
-                                    iOS.
+                                    taalvariëteit aangegeven. Door op een woord in de legenda te klikken, kunnen
+                                    onderzoekers in het CHN de onderliggende gegevens voor de hele curve bekijken. Ook
+                                    hier
+                                    geldt dat bij een tijdsindeling in weken, of met intervallen niet gelijk aan 1, de
+                                    gegevens in CHN met een kleinere datumgranulariteit weergegeven worden.
                                 </li>
                             </ul>
-                        </section>
-                    </TabPanel>
-                    <TabPanel value="1">
-                        <section>
-                            <h2>Algemeen</h2>
-                            <p>
-                                Met Woordpeiler kun je de populariteit van woorden door de tijd heen bekijken, op basis
-                                van teksten uit Nederlandstalige kranten. Zoek op een woord en Woordpeiler visualiseert
-                                de frequentie ervan door de jaren heen in een grafiek, vanaf 2000 tot nu.
 
-                                Het woord <RouterLink to="/grafiek?w=vapen">vapen</RouterLink> kwam bijvoorbeeld in 2014
-                                voor het eerst een enkele keer voor, maar
-                                vanaf 2022 is er een duidelijke stijging te zien van het gebruik van dit woord in de
-                                kranten. De woorden <RouterLink to="/grafiek?w=mondkapje,mondmasker">mondkapje en
-                                    mondmasker</RouterLink> kenden een opvallende piek van 2020 tot 2022
-                                en voor het woord <RouterLink to="/grafiek?w=televisie">televisie</RouterLink> is een
-                                dalende trend zichtbaar.
-                            </p>
+                            <div class="image">
+                                <img src="@/assets/img/slepen.png" alt="Zoomen in de grafiek"
+                                    style="max-width: 700px;" />
+                            </div>
                         </section>
-
                     </TabPanel>
                 </TabPanels>
             </Tabs>
@@ -178,7 +233,6 @@
 </template>
 
 <script setup lang="ts">
-import Panel from 'primevue/panel';
 import ColorPicker from 'primevue/colorpicker';
 import Tabs from 'primevue/tabs';
 import TabList from 'primevue/tablist';
@@ -193,6 +247,7 @@ const canShare = navigator.share != undefined;
 .image {
     display: flex;
     justify-content: center;
+    margin: 1rem 0;
 
     img {
         width: 100%;
