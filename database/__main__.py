4c2b0105478941b8a2a51fc9a07b68cf9166cadb
Vincent Prins <vincent.prins@ivdnt.org>
2025-07-15 17:35:41 +0200

    remove gz extension

--- database/__main__.py
+++ database/__main__.py
@@ -9,13 +9,13 @@ from database.util.timer import timer
 
 
 def initialize(folder: str, config_name: str, ngram: int) -> None:
-    word_path = f"{folder}/{config_name}_word_lookup.tsv.gz"
-    lemma_path = f"{folder}/{config_name}_lemma_lookup.tsv.gz"
-    pos_path = f"{folder}/{config_name}_pos_full_lookup.tsv.gz"
-    words_path = f"{folder}/{config_name}_annotations.tsv.gz"
-    source_path = f"{folder}/{config_name}_metadata_group.tsv.gz"
-    size_path = f"{folder}/{config_name}_size.tsv.gz"
-    freq_path = f"{folder}/{config_name}.tsv.gz"
+    word_path = f"{folder}/{config_name}_word_lookup.tsv"
+    lemma_path = f"{folder}/{config_name}_lemma_lookup.tsv"
+    pos_path = f"{folder}/{config_name}_pos_full_lookup.tsv"
+    words_path = f"{folder}/{config_name}_annotations.tsv"
+    source_path = f"{folder}/{config_name}_metadata_group.tsv"
+    size_path = f"{folder}/{config_name}_size.tsv"
+    freq_path = f"{folder}/{config_name}.tsv"
 
     primary.initialize(
         freq_path=freq_path,
