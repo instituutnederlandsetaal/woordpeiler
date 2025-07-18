f70a814dfdeed4be8cbf88f22ca40e0351aa9f4e
PrinsINT <vincent.prins@ivdnt.org>
2025-07-18 17:25:19 +0200

    typo

--- server/query/trends_query.py
+++ server/query/trends_query.py
@@ -89,7 +89,7 @@ class TrendsQuery(QueryBuilder):
             WITH tc AS (
                 SELECT
                     word_id,
-                    SUM({abs_freq}) / (SELECT SUM(size) FROM {corpus_size} counts {date_filter}) 1e6 AS rel_freq
+                    SUM({abs_freq}) / (SELECT SUM(size) FROM {corpus_size} counts {date_filter}) * 1e6 AS rel_freq
                 FROM {counts_table} counts
                 {date_filter}
                 GROUP BY word_id
