53d9aa448722330526df2bb6ca185c2e39829f5e
Vincent Prins <vincent.prins@ivdnt.org>
2025-09-18 13:06:38 +0200

    Fix basepath when empty

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -4,10 +4,10 @@ import { config } from "@/main"
 export function setAxiosBaseUrl() {
     // On local development, use localhost:8000
     // On production, use host /api
-    if (window.location.hostname == "localhost") {
-        axios.defaults.baseURL = `${window.location.protocol}//${window.location.hostname}:8000/`
+    if (location.hostname == "localhost") {
+        axios.defaults.baseURL = `${location.protocol}//${location.hostname}:8000/`
     } else {
-        axios.defaults.baseURL = `${window.location.protocol}//${window.location.hostname}/${config.basePath}/api`
+        axios.defaults.baseURL = `${location.protocol}//${location.hostname}${config.basePath}/api`
     }
 }
 
